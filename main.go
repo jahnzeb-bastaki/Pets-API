129213edbee303fa7be73fae09cec88513dea24e
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-07-23 20:56:12 -0500

    Connected MongoDB to API

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
+import (
+    "gin-mongo-api/configs" 
+    "github.com/gin-gonic/gin"
+)
 
 func main() {
-  router := gin.Default()
-	
-  router.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "data": "Hello from Gin-gonic & mongoDB",
-    })
-  })
-  router.Run("localhost:6000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    router := gin.Default()
+
+    //run database
+    configs.ConnectDB()
+
+    router.Run("localhost:6000")
 }
