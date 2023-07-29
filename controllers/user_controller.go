413f065c2aa9507cd7e1f44968098bd059ff0405
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-07-29 01:22:34 -0500

    Created Edit Route

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -71,3 +71,44 @@ func GetUser() gin.HandlerFunc {
 			c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": user}})
 	}
 }
+
+func EditAUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			userId := c.Param("userId")
+			var user models.User
+			defer cancel()
+			objId, _ := primitive.ObjectIDFromHex(userId)
+
+			//validate the request body
+			if err := c.BindJSON(&user); err != nil {
+					c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+					return
+			}
+
+			//use the validator library to validate required fields
+			if validationErr := validate.Struct(&user); validationErr != nil {
+					c.JSON(http.StatusBadRequest, responses.UserResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": validationErr.Error()}})
+					return
+			}
+
+			update := bson.M{"name": user.Name, "location": user.Location, "title": user.Title}
+			result, err := userCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+			if err != nil {
+					c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+					return
+			}
+
+			//get updated user details
+			var updatedUser models.User
+			if result.MatchedCount == 1 {
+					err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&updatedUser)
+					if err != nil {
+							c.JSON(http.StatusInternalServerError, responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+							return
+					}
+			}
+
+			c.JSON(http.StatusOK, responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": updatedUser}})
+	}
+}
