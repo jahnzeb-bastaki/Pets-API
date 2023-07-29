081dbf9ac7f00c9393d676cd458bcaa7495d33ab
Jahnzeb Bastaki <jahnzebbastaki@gmail.com>
2023-07-29 16:57:36 -0500

    Updated setup.go

--- configs/setup.go
+++ configs/setup.go
@@ -12,29 +12,24 @@ import (
 // Connects Mongo DB using EnvMongoURI function while also disconnecting 
 // when there is trouble connecting within 10 seconds
 func ConnectDB() *mongo.Client  {
-	// context that has a 10 second timeout connected to it
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	// use 'client' as the variable that connects the DB
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
-  if err != nil {
-      log.Fatal(err)
-  }
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-  //ping the database
-  err = client.Ping(ctx, nil)
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
 	if err != nil {
-    log.Fatal(err)
-  }
+			log.Fatal(err)
+	}
 
-  fmt.Println("Connected to MongoDB")
-  return client
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+			log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
 }
 
 //Client instance
@@ -42,6 +37,6 @@ var DB *mongo.Client = ConnectDB()
 
 //getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-    collection := client.Database("golangAPI").Collection(collectionName)
-    return collection
+	collection := client.Database("golangAPI").Collection(collectionName)
+	return collection
 }
